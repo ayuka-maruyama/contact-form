77fa4fd898c14fd64ce267d9475ddbcd4d17b6b8
ayuka <basketball110512@gmail.com>
2024-03-25 04:18:28 +0900

    Laravel演習0-6完成

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -32,10 +32,12 @@
                     </div>
                     <div class="form__group-content">
                         <div class="form__input--text">
-                            <input type="text" name="name" placeholder="テスト太郎" />
+                            <input type="text" name="name" placeholder="テスト太郎" value="{{ old('name') }}" />
                         </div>
                         <div class="form__error">
-                            <!-- バリデーション機能を実装したら記述する -->
+                            @error('name')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -46,10 +48,12 @@
                     </div>
                     <div class="form__group-content">
                         <div class="form__input--text">
-                            <input type="email" name="email" placeholder="test@example.com" />
+                            <input type="email" name="email" placeholder="test@example.com" value="{{ old('email') }}"/>
                         </div>
                         <div class="form__error">
-                            <!-- バリデーション機能を実装したら記述する -->
+                            @error('email')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -60,10 +64,12 @@
                     </div>
                     <div class="form__group-content">
                         <div class="form__input--text">
-                            <input type="tel" name="tel" placeholder="09012345678" />
+                            <input type="tel" name="tel" placeholder="09012345678" value="{{ old('tel') }}"/>
                         </div>
                         <div class="form__error">
-                            <!-- バリデーション機能を実装したら記述する -->
+                            @error('tel')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
